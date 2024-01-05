5c5f4b8ac9c8ed69ad5c627338b8a98c0c5ee2e0
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-01-05 16:13:01 +0100

    Work with DNES

--- Valgundersøgelsen.R
+++ Valgundersøgelsen.R
@@ -19,8 +19,7 @@ dnes19 <- import_dnes19 %>%
   select(!weight_2) %>% 
   select(!weight_3) %>% 
   select(!weight_4) %>% 
-  select(!weight_5) %>% 
-  select()
+  select(!weight_5)
 
 dnes19 <- import_dnes19 %>% 
   # mutate_if(is.character, factor) %>% 
@@ -42,7 +41,7 @@ dnes19 %>%
   geom_point() +
   geom_smooth(aes(color = region)) +
   facet_wrap(.~region) +
-  scale_fill_brewer(palette = "Pastel2")
+  scale_color_brewer(palette = "Dark2")
 
 RColorBrewer::display.brewer.all()
 
